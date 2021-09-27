b5f7e7ef089982780b27d1d237881cdf824f7dc9
Blue Sky <alister11david@gmail.com>
2021-09-27 17:42:11 +0800

    Added firebase auth API

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -93,11 +93,80 @@ class RegistrationController extends AbstractController
 
             return $this->json($errors, 422);
         } else {
+            $authyApiUser = $this->authyApi->registerUser('test@test.com', $data['phone'], $data['country_code'], false);
+
+            if ($authyApiUser->ok()) {
+
+                $authyId = $authyApiUser->id();
+                $newUser->setAuthyId($authyId);
+
+                $em->persist($newUser);
+                $em->flush();
+
+                return $this->sendSMS($newUser);
+            }
+
+            return $this->json($authyApiUser->message(), 422);
+        }
+    }
+
+    /**
+     * @Route("/firebaseLogin", name="app_firebase_login", methods={"POST"})
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function firebaseLogin(
+        Request $request,
+        ValidatorInterface $validator,
+        UserRepository $userRepository,
+        EntityManagerInterface $em
+    ): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $check = $userRepository->findOneBy(['phone' => $data['phone']]);
+        if ($check && in_array('super_admin', $check->getRoles())) {
+            $this->get('session')->set('user', $check);
+            return $this->json([
+                'data' => [
+                    'phone' => $check->getPhone()
+                ],
+                'Cookie' => 'PHPSESSID=' . session_id()
+            ], 200);
+        }
+
+        if ($check) {
+            if(isset($data['uuid'])) {
+                $check->setUuid($data['uuid']);
+                $em->persist($check);
+                $em->flush();
+            }
+
+            return $this->sendFirebaseSMS($check);
+        }
+
+        $newUser = new User();
+        $newUser->setPhone($data['phone']);
+        $newUser->setCountryCode($data['country_code']);
+        $newUser->setUuid(isset($data['uuid']) ?: null);
+        $violations = $validator->validate($newUser);
+
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $violation) {
+                $errors[$violation->getPropertyPath()] = $violation->getMessage();
+            }
+
+            return $this->json($errors, 422);
+        } else {
             $newUser->setAuthyId(rand(10,1000));
             $em->persist($newUser);
             $em->flush();
 
-            return $this->sendSMS($newUser);
+            return $this->sendFirebaseSMS($newUser);
         }
     }
 
@@ -106,6 +175,29 @@ class RegistrationController extends AbstractController
      * @return JsonResponse
      */
     public function sendSMS($user): JsonResponse
+    {
+        $sms = $this->authyApi->requestSms($user->getAuthyId(), ['force' => true]);
+
+        if ($sms->ok()) {
+            $this->get('session')->set('user', $user);
+
+            return $this->json([
+                'data' => [
+                    'phone' => $user->getPhone()
+                ],
+                'Cookie' => 'PHPSESSID=' . session_id()
+            ], 200);
+        }
+
+        return $this->json($sms->message(), 400);
+
+    }
+
+    /**
+     * @param $user
+     * @return JsonResponse
+     */
+    public function sendFirebaseSMS($user): JsonResponse
     {
         $this->get('session')->set('user', $user);
 
@@ -128,6 +220,16 @@ class RegistrationController extends AbstractController
     }
 
     /**
+     * @Route("/verify/firebaseResend", name="resend_firebase_code", methods={"POST"})
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function firebaseResendCode(): JsonResponse
+    {
+        return $this->sendFirebaseSMS($this->getUser());
+    }
+
+    /**
      * @Route("/verify/code", name="verify_code", methods={"POST"})
      * @param Request $request
      * @param UserRepository $userRepository
@@ -136,6 +238,55 @@ class RegistrationController extends AbstractController
      * @throws Exception
      */
     public function verifyCode(
+        Request $request,
+        UserRepository $userRepository,
+        EntityManagerInterface $em
+    ): JsonResponse
+    {
+        $dataRequest = json_decode($request->getContent(), true);
+        // Get data from session
+        $data = $this->getUser();
+        if (in_array('super_admin', $data->getRoles())) {
+            $user = $em->getRepository(User::class)->findOneBy(['phone' => $data->getPhone()]);
+            $token = bin2hex(random_bytes(16));
+            $user->setApiToken($token);
+            $user->setLastLogin(new DateTime(date('Y-m-d H:i:00')));
+            $user->setTimezone($dataRequest['timezone']);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->json($user->getApiToken(), 200);
+        }
+
+        if($data) {
+            $verification = $this->authyApi->verifyToken($data->getAuthyId(), $dataRequest['verify_code']);
+            if ($verification->ok()) {
+                $user = $userRepository->findOneBy(['phone' => $data->getPhone()]) ?? $data;
+
+                # Create new API key (token)
+                $token = bin2hex(random_bytes(16));
+                $user->setApiToken($token);
+                $user->setLastLogin(new DateTime(date('Y-m-d H:i:00')));
+                $user->setTimezone($dataRequest['timezone']);
+                $em->persist($user);
+                $em->flush();
+
+                return $this->json($user->getApiToken(), 200);
+            }
+        }
+
+        return $this->json(['error' => !empty($data) ? $verification->message() : 'Incorrect PHPSESSID'], 422);
+    }
+
+    /**
+     * @Route("/verify/firebaseCode", name="verify_firebase_code", methods={"POST"})
+     * @param Request $request
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function verifyFirebaseCode(
         Request $request,
         UserRepository $userRepository,
         EntityManagerInterface $em
